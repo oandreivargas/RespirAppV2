336cec11d156d8f3e1bdbc323f16d05afc8bc794
oandreivargas <andrei-94@hotmail.com>
2026-09-19 17:43:27 -0300

    Update cache name from 'respira-v20' to 'respira-v21'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'respira-v20';
+const CACHE_NAME = 'respira-v21';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
  
 self.addEventListener('install', e => {
