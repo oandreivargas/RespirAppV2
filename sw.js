aa2643cf6dcbd82aca69776231ba413b50506602
oandreivargas <andrei-94@hotmail.com>
2026-09-19 15:22:31 -0300

    Update CACHE_NAME to respira-v10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'respira-v9';
+const CACHE_NAME = 'respira-v10';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
  
 self.addEventListener('install', e => {
@@ -27,4 +27,3 @@ self.addEventListener('fetch', e => {
     }).catch(() => caches.match(e.request))
   );
 });
- 
